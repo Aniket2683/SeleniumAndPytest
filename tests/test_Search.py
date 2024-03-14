be83c6f0e98935f2a24f9c13997a01621acc7201
Aniket Jaiswal <jaiswalaniket302@gmail.com>
2024-03-14 22:46:58 +0530

    running multiple selenium test

--- tests/test_Search.py
+++ tests/test_Search.py
@@ -14,3 +14,27 @@ def test_search_for_a_valid_product():
     assert driver.find_element(By.LINK_TEXT,"HP LP3065").is_displayed()
     time.sleep(2)
     driver.quit()
+
+
+def test_search_for_an_invalid_product():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://tutorialsninja.com/demo/")
+    driver.maximize_window()
+    driver.find_element(By.NAME,"search").send_keys("Honda")
+    driver.find_element(By.XPATH,"//button[contains(@class,'btn-default')]").click()
+    expected_text = "There is no product that matches the search criteria."
+    assert driver.find_element(By.XPATH,"//input[@id='button-search']//following-sibling::p").text.__eq__(expected_text)
+    time.sleep(2)
+    driver.quit()
+
+
+def test_search_without_providing_any_product():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://tutorialsninja.com/demo/")
+    driver.maximize_window()
+    driver.find_element(By.NAME,"search").send_keys("")
+    driver.find_element(By.XPATH,"//button[contains(@class,'btn-default')]").click()
+    expected_text = "There is no product that matches the search criteria."
+    assert driver.find_element(By.XPATH,"//input[@id='button-search']//following-sibling::p").text.__eq__(expected_text)
+    time.sleep(2)
+    driver.quit()
