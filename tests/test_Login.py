7faa2d1cc51ee3ac5345cdf6d7647e6807f4246f
Aniket Jaiswal <jaiswalaniket302@gmail.com>
2024-03-16 01:24:50 +0530

    allure reports generated

--- tests/test_Login.py
+++ tests/test_Login.py
@@ -6,35 +6,26 @@ import time
 import pytest
 
 
-@pytest.fixture()
-def setup_and_teardown():
-    global driver
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.maximize_window()
-    driver.get("https://tutorialsninja.com/demo/")
-    yield driver
-    driver.quit()
+@pytest.mark.usefixtures("setup_and_teardown")
+class TestLogin:
+    def test_login_with_valid_credentials(self):
+        self.driver.find_element(By.XPATH, "//span[text()='My Account']").click()
+        self.driver.find_element(By.LINK_TEXT, "Login").click()
+        self.driver.find_element(By.ID, "input-email").send_keys("aniketjai@gmail.com")
+        time.sleep(1)
+        self.driver.find_element(By.ID, "input-password").send_keys("121212")
+        self.driver.find_element(By.XPATH, "//input[@value='Login']").click()
+        time.sleep(1)
+        assert self.driver.find_element(By.LINK_TEXT, "Edit your account information").is_displayed()
 
-
-def test_login_with_valid_credentials(setup_and_teardown):
-
-    driver.find_element(By.XPATH, "//span[text()='My Account']").click()
-    driver.find_element(By.LINK_TEXT, "Login").click()
-    driver.find_element(By.ID, "input-email").send_keys("aniketjai@gmail.com")
-    time.sleep(1)
-    driver.find_element(By.ID, "input-password").send_keys("121212")
-    driver.find_element(By.XPATH, "//input[@value='Login']").click()
-    time.sleep(1)
-    assert driver.find_element(By.LINK_TEXT, "Edit your account information").is_displayed()
-
-
-def test_login_without_entering_credentials(setup_and_teardown):
-    driver.find_element(By.XPATH, "//span[text()='My Account']").click()
-    driver.find_element(By.LINK_TEXT, "Login").click()
-    driver.find_element(By.ID, "input-email").send_keys("")
-    time.sleep(1)
-    driver.find_element(By.ID, "input-password").send_keys("")
-    driver.find_element(By.XPATH, "//input[@value='Login']").click()
-    time.sleep(1)
-    expected_warning = "Warning: No match for E-Mail Address and/or Password."
-    assert driver.find_element(By.XPATH, "//div[@id='account-login']/div[1]").text.__contains__(expected_warning)
+    def test_login_without_entering_credentials(self):
+        self.driver.find_element(By.XPATH, "//span[text()='My Account']").click()
+        self.driver.find_element(By.LINK_TEXT, "Login").click()
+        self.driver.find_element(By.ID, "input-email").send_keys("")
+        time.sleep(1)
+        self.driver.find_element(By.ID, "input-password").send_keys("")
+        self.driver.find_element(By.XPATH, "//input[@value='Login']").click()
+        time.sleep(1)
+        expected_warning = "Warning: No match for E-Mail Address and/or Password."
+        assert self.driver.find_element(By.XPATH, "//div[@id='account-login']/div[1]").text.__contains__(
+            expected_warning)
--- tests/test_Search_with_fixture.py
+++ tests/test_Search_with_fixture.py
@@ -6,36 +6,26 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 
-@pytest.fixture
-def setup_and_teardown():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get("https://tutorialsninja.com/demo/")
-    driver.maximize_window()
-    yield driver
-    driver.quit()
+@pytest.mark.usefixtures("setup_and_teardown")
+class TestSearch:
+    def test_search_for_a_valid_product(self):
+        self.driver.find_element(By.NAME, "search").send_keys("HP")
+        self.driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
+        assert self.driver.find_element(By.LINK_TEXT, "HP LP3065").is_displayed()
+        time.sleep(2)
 
+    def test_search_for_an_invalid_product(self):
+        self.driver.find_element(By.NAME, "search").send_keys("Honda")
+        self.driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
+        expected_text = "There is no product that matches the search criteria."
+        assert self.driver.find_element(By.XPATH, "//input[@id='button-search']//following-sibling::p").text.__eq__(
+            expected_text)
+        time.sleep(2)
 
-def test_search_for_a_valid_product(setup_and_teardown):
-    driver = setup_and_teardown
-    driver.find_element(By.NAME, "search").send_keys("HP")
-    driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
-    assert driver.find_element(By.LINK_TEXT, "HP LP3065").is_displayed()
-    time.sleep(2)
-
-
-def test_search_for_an_invalid_product(setup_and_teardown):
-    driver = setup_and_teardown
-    driver.find_element(By.NAME, "search").send_keys("Honda")
-    driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
-    expected_text = "There is no product that matches the search criteria."
-    assert driver.find_element(By.XPATH, "//input[@id='button-search']//following-sibling::p").text == expected_text
-    time.sleep(2)
-
-
-def test_search_without_providing_any_product(setup_and_teardown):
-    driver = setup_and_teardown
-    driver.find_element(By.NAME, "search").send_keys("")
-    driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
-    expected_text = "There is no product that matches the search criteria."
-    assert driver.find_element(By.XPATH, "//input[@id='button-search']//following-sibling::p").text == expected_text
-    time.sleep(2)
+    def test_search_without_providing_any_product(self, ):
+        self.driver.find_element(By.NAME, "search").send_keys("")
+        self.driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
+        expected_text = "There is no product that matches the search criteria."
+        assert self.driver.find_element(By.XPATH, "//input[@id='button-search']//following-sibling::p").text.__eq__(
+            expected_text)
+        time.sleep(2)
