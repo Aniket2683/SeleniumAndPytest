c782db8dfd615cf4a203cdf6995457ff7800c5ac
Aniket Jaiswal <jaiswalaniket302@gmail.com>
2024-03-18 21:34:25 +0530

    allure reports with ss

--- tests/test_Search_with_fixture.py
+++ tests/test_Search_with_fixture.py
@@ -1,8 +1,8 @@
 import time
+
+import allure
 import pytest
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from allure_commons.types import AttachmentType
 from selenium.webdriver.common.by import By
 
 
@@ -18,14 +18,13 @@ class TestSearch:
         self.driver.find_element(By.NAME, "search").send_keys("Honda")
         self.driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
         expected_text = "There is no product that matches the search criteria."
-        assert self.driver.find_element(By.XPATH, "//input[@id='button-search']//following-sibling::p").text.__eq__(
-            expected_text)
+        assert self.driver.find_element(By.XPATH, "//input[@id='button-search']//following-sibling::p").text.__eq__(expected_text)
         time.sleep(2)
 
     def test_search_without_providing_any_product(self, ):
         self.driver.find_element(By.NAME, "search").send_keys("")
         self.driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
         expected_text = "There is no product that matches the search criteria."
-        assert self.driver.find_element(By.XPATH, "//input[@id='button-search']//following-sibling::p").text.__eq__(
-            expected_text)
+        assert self.driver.find_element(By.XPATH, "//input[@id='button-search']//following-sibling::p").text.__eq__(expected_text)
+        allure.attach(self.driver.get_screenshot_as_png(),name="search_without_providing_any_product",attachment_type=AttachmentType.PNG)
         time.sleep(2)
